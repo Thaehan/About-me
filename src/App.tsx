95190826d4bb4f9d19902a0f570c47dd698d04a8
Do Van Dat <thaehan0611@gmail.com>
2023-03-09 17:19:26 +0700

    Clean main branch

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import RootRoutes from '@Routes/RootRoutes'
-
-export default function App() {
-  return <RootRoutes />
-}
--- src/Screens/HelpScreen.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function HelpScreen() {
-  return <div>HelpScreen</div>
-}
